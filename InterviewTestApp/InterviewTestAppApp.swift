5fcd92b3a251df78afe81e8adcd700ce46fee3c4
Hari-Rait <86234153+Hari-Rait@users.noreply.github.com>
2024-03-19 17:54:43 +0100

    Initial Commit

---
+++ InterviewTestApp/InterviewTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewTestAppApp.swift
+//  InterviewTestApp
+//
+//  Created by Hari Rait on 19.03.24.
+//
+
+import SwiftUI
+
+@main
+struct InterviewTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
